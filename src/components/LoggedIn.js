8a6f7d2a83a4c6acab59534db851f3728312bebe
alexis-tao <tao.alexis@gmail.com>
2019-10-27 15:12:38 -0400

    add chart

--- src/components/LoggedIn.js
+++ src/components/LoggedIn.js
@@ -2,12 +2,14 @@ import React from 'react';
 import styled from 'styled-components';
 import hash from '../hash';
 import axios from 'axios';
+import { ResponsiveLine } from '@nivo/line';
 
 const Page = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-around;
-  align-items: center;
+  align-items: left;
+  padding: 30px 50px;
   min-height: 100vh;
   width: 100%;
   background-color: #262626;
@@ -20,8 +22,10 @@ const Title = styled.div`
   letter-spacing: 3px;
 `;
 
-const Track = styled.div`
-  color: #ffffff;
+const ChartWrapper = styled.div`
+  height: 500px;
+  width: 700px;
+  background-color: #ffffff;
 `;
 
 export class LoggedIn extends React.Component {
@@ -34,7 +38,9 @@ export class LoggedIn extends React.Component {
   }
 
   componentDidMount() {
-    let token = hash.access_token;
+    let token =
+      hash.access_token ||
+      'BQC87KcimI4x5XZp0ikuRCZDCz5fVy7pLEjdbCDBOEkQ7qBngUsC40LqwIIcVH-L-nx0UxMA_Dc91IxhGEwmXGFRFm0KzcwHtBiEFuoZj9G0w8XXGHwpg_bCktc4GcUyKkaz-3LJ1pcMg8wCRIPoNN-DL84vr68';
 
     if (token) {
       this.setState({ token }, this.getRecentTracks);
@@ -91,15 +97,44 @@ export class LoggedIn extends React.Component {
 
   render() {
     const { tracks } = this.state;
-    console.log(tracks);
+    const dataInner = tracks.reduce((acc, track) => {
+      acc.push({ x: track.name, y: track.energy });
+      return acc;
+    }, []);
+
+    const data = [{ id: 'your energy', data: dataInner }];
+
     return (
       <Page>
-        <Title>you're logged in!</Title>
-        {tracks.map(track => (
-          <div>
-            <Track>{track.name}</Track>
-          </div>
-        ))}
+        <Title>energy</Title>
+        <ChartWrapper>
+          <ResponsiveLine
+            data={data}
+            margin={{ top: 50, right: 50, bottom: 50, left: 80 }}
+            xScale={{ type: 'point' }}
+            yScale={{ type: 'linear', stacked: true, min: 'auto', max: 'auto' }}
+            axisTop={null}
+            axisRight={null}
+            axisBottom={null}
+            axisLeft={{
+              orient: 'left',
+              tickSize: 5,
+              tickPadding: 5,
+              tickRotation: 0,
+              legend: 'energy',
+              legendOffset: -45,
+              legendPosition: 'middle'
+            }}
+            colors={['#fc7978', '#94ceca']}
+            pointSize={10}
+            pointColor={{ theme: 'background' }}
+            pointBorderWidth={2}
+            pointBorderColor={{ from: 'serieColor' }}
+            pointLabel="y"
+            pointLabelYOffset={-12}
+            useMesh={true}
+          />
+        </ChartWrapper>
       </Page>
     );
   }
